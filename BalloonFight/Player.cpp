943d20adf913cf8b0f13a19bcac96a21a6eeca48
Matsuda-Syoma <122336837+Matsuda-Syoma@users.noreply.github.com>
2023-06-13 13:35:01 +0900

    Player仮

---
+++ BalloonFight/Player.cpp
@@ -0,0 +1,23 @@
+#include "common.h"
+#include "Player.h"
+#include "PAD_INPUT.h"
+#include <math.h>
+
+Player::Player() {
+	flg = true;
+	life = 3;
+	x = 40;
+	y = SCREEN_WIDTH - 96;
+	w = WIDTH;
+	h = HEIGHT;
+	speedX = 0;
+	speedY = 0;
+	speedMax = 3;
+	LoadImages();
+}
+
+void Player::LoadImages() {
+	for (int i = 0; i < 3; i++) {
+		images[i] = 0;
+	}
+}
---
+++ BalloonFight/Player.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "BoxCollider.h"
+class Player : public BoxCollider
+{
+private:
+	bool flg;					// プレイヤーが動かせるか
+	int life;					// プレイヤーの残機
+	float x, y;					// プレイヤーの座標
+	float w, h;					// プレイヤーの大きさ
+	float speedX, speedY;		// プレイヤーの速度
+	float speedMax;				// プレイヤーの最大速度
+	float imageX, imageY;		// プレイヤーの画像の座標
+	int images[3];
+
+	static const int WIDTH = 40;
+	static const int HEIGHT = 40;
+
+public:
+	Player();					// コンストラクタ
+	~Player();					// デストラクタ
+	void Update();				// 更新処理
+		
+	void LoadImages();	// 画像読み込み
+	void DeleteImages();	// 画像削除
+};
+
