16d31ee0a820c774641115812f6862169d3cccac
Matsuda-Syoma <122336837+Matsuda-Syoma@users.noreply.github.com>
2023-06-15 12:17:28 +0900

    反発係数実装(バグあり)

--- BalloonFight/Player.cpp
+++ BalloonFight/Player.cpp
@@ -15,7 +15,7 @@ Player::Player()			// コンストラクタ
 	inertiaY = 0;
 	speedX = 0;
 	speedY = 0;
-	speedMax = 3;
+	speedMax = 3.0f;
 	LoadImages();
 }
 
@@ -46,6 +46,9 @@ void Player::Update()		// プレイヤーの更新処理
 	if (y > SCREEN_HEIGHT) {
 		y = 90;
 	}
+	if (y < 0) {
+		inertiaY *= -0.8f;
+	}
 
 	// スティック操作時に0から1の間が出るようにする------------後で整数にする
 	//speedX = round(((float)PAD_INPUT::GetPadThumbLX() / 32767) * 100) / 100;
@@ -53,35 +56,52 @@ void Player::Update()		// プレイヤーの更新処理
 
 	// 落下処理
 	if (inertiaY < 350) {
-		inertiaY += 0.8;
+		inertiaY += 1.5;
 	}
 
+	// Aボタンを押したときに上に加速
 	if (PAD_INPUT::GetKeyFlg(XINPUT_BUTTON_A)) {
-		inertiaY -= 25+(inertiaY / 4);
+		if (inertiaY > 0) {
+			inertiaY -= 25 + (inertiaY / 4);
+		}
+		else {
+			inertiaY -= 50;
+		}
+
+		//右方向に入力したままAボタンを押したなら右に加速
+		if (round(((float)PAD_INPUT::GetPadThumbLX() / 32767) * 100) / 100 >= 0.1) {
+			inertiaX += 1.0f;
+			// 加速したときに速度上限なら速度を上限で固定
+			if (inertiaX > speedMax) {
+				inertiaX = speedMax;
+			}
+		}
+		//左方向に入力したままAボタンを押したなら左に加速
+		if (round(((float)PAD_INPUT::GetPadThumbLX() / 32767) * 100) / 100 <= -0.1) {
+			inertiaX -= 1.0f;
+			// 加速したときに速度上限なら速度を上限で固定
+			if (inertiaX < -speedMax) {
+				inertiaX = -speedMax;
+			}
+		}
 	}
 
 	y += inertiaY / (FRAMERATE * 1.0f);		// 仮の重力(フレーム数 * 風船の数)
-
-	// プレイヤーの移動処理  -----(仮)
-	x += speedX;
+	x += inertiaX;
 
 	//右
-	//if (round(((float)PAD_INPUT::GetPadThumbLX() / 32767) * 100) / 100 < 1 && speedX < 5.6f) {
+	if (round(((float)PAD_INPUT::GetPadThumbLX() / 32767) * 100) / 100 >= 0.1 && inertiaX < speedMax) {
 
-	//	speed += 0.1f;
+		inertiaX += 0.001f;
 
-	//}
-	//else if (gPlayer.speed > 0.0f) {
-	//	gPlayer.speed -= 0.2f;
-	//}
+	}
 
-	////左
-	//if (gNowKey & PAD_INPUT_LEFT && gPlayer.speed > -5.6f) {
-	//	gPlayer.speed -= 0.1f;
-	//}
-	//else if (gPlayer.speed < 0.0f) {
-	//	gPlayer.speed += 0.2f;
-	//}
+	//左
+	if (round(((float)PAD_INPUT::GetPadThumbLX() / 32767) * 100) / 100 <= -0.1 && inertiaX > -speedMax) {
+
+		inertiaX -= 0.001f;
+
+	}
 }
 
 void Player::Draw() const
