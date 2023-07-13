1362f3ce108e37cf283f08cfbbc71aafcba8fe2c
hibiki1009 <122337061+hibiki1009@users.noreply.github.com>
2023-07-13 13:40:29 +0900

    タイトル描画

--- BalloonFight/Title.cpp
+++ BalloonFight/Title.cpp
@@ -2,10 +2,19 @@
 
 Title::Title()
 {
+	LoadImage();
+	CursorNum = 0;
+	WaitTime = 0;
+
+	TitleImg_Logo = LoadGraph("Resources/images/Title/Title_Logo.png");
+	TitleImg_Select = LoadGraph("Resources/images/Title/Title_ModeSelect.png");
+	TitleImg_Credit = LoadGraph("Resources/images/Title/Title_Credit.png");
+	/*TitleImg_Cursor = LoadGraph("Resources/images/Title/Title_CursorAnimation.png");*/
 }
 
 Title::~Title()
 {
+
 }
 
 AbstractScene* Title::Update()
@@ -15,5 +24,28 @@ AbstractScene* Title::Update()
 
 void Title::Draw() const
 {
-	DrawString(100, 100, "test title", 0xff0000);
+	DrawRotaGraph(330, 140, 1, 0, TitleImg_Logo, TRUE);
+	DrawRotaGraph(340, 330, 1, 0, TitleImg_Select, TRUE);
+	DrawRotaGraph(340, 450, 1, 0, TitleImg_Credit, TRUE);
+
+	DrawRotaGraph(340, CursorY, 1.0f, 0, TitleImg_Cursor[CursorNum], TRUE, 0);
+
+	/*DrawRotaGraph(imageX, imageY, 1.0f, 0, images[AnimImg], true, 0);*/
+
+}
+
+void Title::LoadImage()
+{
+	LoadDivGraph("Resources/images/Title/Title_CursorAnimation.png", 4, 4, 1, 64, 64, TitleImg_Cursor);
+}
+
+bool Title::CAnim_Play()
+{
+	if (++WaitTime % 3 == 0) {
+		CursorNum++;
+	}
+	if (CursorNum > 3) {
+		return true;
+	}
+	return false;
 }
--- BalloonFight/Title.h
+++ BalloonFight/Title.h
@@ -1,9 +1,20 @@
 #pragma once
 #include "AbstractScene.h"
 class Title : public AbstractScene {
+private:
+	int TitleImg_Logo;					// タイトルロゴ画像
+	int TitleImg_Select;				// タイトルセレクト画像
+	int TitleImg_Credit;				// タイトルクレジット画像
+	int TitleImg_Cursor[4];				// タイトルカーソル画像
+	int CursorNum;
+	int CursorY = 330;
+
+	int WaitTime;
 public:
 	Title();							// コンストラクタ
 	~Title();							// デストラクタ
 	AbstractScene* Update() override;	// 描画以外の更新を実行
 	void Draw() const override;			// 描画に関することを実装
+	void LoadImage();
+	bool CAnim_Play();
 };
